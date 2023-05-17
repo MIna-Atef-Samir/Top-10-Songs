90c6897ec7ab407bc0ad368ff110ece38486d6d0
Mina Atef <mina2000atef@gmail.com>
2023-05-17 22:10:04 +0300

    last edit

--- with js/main.js
+++ with js/main.js
@@ -1,9 +1,16 @@
 let btn = document.querySelector(".btn");
 let list = document.querySelector(".list");
 let two = document.querySelector(".two");
+let ddclose = document.querySelector(".close")
 
-document.querySelector(".btn").addEventListener("click", () => {
+btn.addEventListener("click", () => {
     two.style.display = "none";
     list.style.display = "flex";
+
     console.log("clicked");
+})
+
+ddclose.addEventListener("click", ()=>{
+    two.style.display = "block";
+    list.style.display = "none";
 })
